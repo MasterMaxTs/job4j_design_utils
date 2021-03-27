fac09e9ee43bdb83f276ab712229b2f9a2f486cb
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-27 10:33:15 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#501422]. Изменен алгоритм в методе hasNext(): убрана связь, изменяющая состояние полей объекта итерации; подправлен алгоритм в методе next()

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -3,6 +3,10 @@ package ru.job4j.it;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Класс демонстрирует создание итератора для двумерного массива int[][]
+ * @version 2.0
+ */
 public class MatrixIt implements Iterator<Integer> {
     private final int[][] data;
     private int row = 0;
@@ -12,21 +16,39 @@ public class MatrixIt implements Iterator<Integer> {
         this.data = data;
     }
 
+    /**
+     * Реализация метода hasNext() интерфейса Iterator<Integer>.
+     * В методе проверяется наличие пустых вложенных массивов
+     * @return возвращает условие, подтверждающее наличие текущего элемента
+     */
     @Override
     public boolean hasNext() {
-        while (row < data.length
-                && column == data[row].length) {
-            column = 0;
-            row++;
+        int count = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].length == 0) {
+                count++;
+            }
         }
-        return row < data.length
-                && column < data[row].length;
+        return count != data.length && row < data.length;
     }
 
+    /**
+     * Реализация метода next() интерфейса Iterator<Integer>
+     * @return возвращает текущий элемент двумерного массива.
+     * Если следующий элемент null, то такой элемент пропускается и
+     * курсор переводится на следующий not null элемент.
+     */
     @Override
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
+        }
+        if (column > data[row].length - 1) {
+            column = 0;
+            row++;
+            while (data[row].length == 0) {
+                row++;
+            }
         }
         return data[row][column++];
     }
