b5162f3f2f6274830ff73cefe8f14d88c4e1e692
Maxim Tsurkanov <max86ts@gmail.com>
2021-03-27 15:59:56 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#501422]

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -5,7 +5,6 @@ import java.util.NoSuchElementException;
 
 /**
  * Класс демонстрирует создание итератора для двумерного массива int[][]
- * @version 2.0
  */
 public class MatrixIt implements Iterator<Integer> {
     private final int[][] data;
@@ -16,39 +15,21 @@ public class MatrixIt implements Iterator<Integer> {
         this.data = data;
     }
 
-    /**
-     * Реализация метода hasNext() интерфейса Iterator<Integer>.
-     * В методе проверяется наличие пустых вложенных массивов
-     * @return возвращает условие, подтверждающее наличие текущего элемента
-     */
     @Override
     public boolean hasNext() {
-        int count = 0;
-        for (int i = 0; i < data.length; i++) {
-            if (data[i].length == 0) {
-                count++;
-            }
+        while (row < data.length
+                && column == data[row].length) {
+            column = 0;
+            row++;
         }
-        return count != data.length && row < data.length;
+        return row < data.length
+                && column < data[row].length;
     }
 
-    /**
-     * Реализация метода next() интерфейса Iterator<Integer>
-     * @return возвращает текущий элемент двумерного массива.
-     * Если следующий элемент null, то такой элемент пропускается и
-     * курсор переводится на следующий not null элемент.
-     */
     @Override
     public Integer next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
-        }
-        if (column > data[row].length - 1) {
-            column = 0;
-            row++;
-            while (data[row].length == 0) {
-                row++;
-            }
         }
         return data[row][column++];
     }
