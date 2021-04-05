cc187e59960d73c546c36418ba486661e319820e
Maxim Tsurkanov <max86ts@gmail.com>
2021-04-05 21:56:03 +0300

    1. Динамический список на массиве. [#501412]

--- src/main/java/ru/job4j/generics/DynamicList.java
+++ src/main/java/ru/job4j/generics/DynamicList.java
@@ -1,2 +1,52 @@
-package ru.job4j.generics;public class DynamicList {
+package ru.job4j.generics;
+
+import java.util.*;
+
+public class DynamicList<T> implements Iterable<T> {
+    private final int minCapacity = 3;
+    private Object[] elementData = new Object[minCapacity];
+    private int size = 0;
+    private int modCount = 0;
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return (T) elementData[index];
+    }
+
+    public void add(T model) {
+        if (size == elementData.length) {
+            int newCapacity = (size * 3) / 2 + 1;
+            elementData = Arrays.copyOf(elementData, newCapacity);
+        }
+        elementData[size++] = model;
+        modCount++;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int expectedModCount = modCount;
+            private int cell = 0;
+
+            @Override
+            public boolean hasNext() {
+                while (cell < elementData.length
+                        && elementData[cell] == null) {
+                    cell++;
+                }
+                return cell < elementData.length;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return (T) elementData[cell++];
+            }
+        };
+    }
 }
--- src/test/java/ru/job4j/generics/DynamicListTest.java
+++ src/test/java/ru/job4j/generics/DynamicListTest.java
@@ -1,4 +1,72 @@
-import static org.junit.Assert.*;
+package ru.job4j.generics;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class DynamicListTest {
-  
+
+    @Test
+    public void whenAddThenGet() {
+        DynamicList<String> array = new DynamicList<>();
+        array.add("first");
+        String rsl = array.get(0);
+        assertThat(rsl, is("first"));
+    }
+
+    @Test
+    public void whenAddThenIt() {
+        DynamicList<String> array = new DynamicList<>();
+        array.add("first");
+        String rsl = array.iterator().next();
+        assertThat(rsl, is("first"));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetEmpty() {
+        DynamicList<String> array = new DynamicList<>();
+        array.get(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetOutBound() {
+        DynamicList<String> array = new DynamicList<>();
+        array.add("first");
+        array.get(1);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenGetEmptyFromIt() {
+        DynamicList<String> array = new DynamicList<>();
+        array.iterator().next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        DynamicList<String> array = new DynamicList<>();
+        array.add("first");
+        Iterator<String> it = array.iterator();
+        array.add("second");
+        it.next();
+    }
+
+    @Test
+    public void whenAddElGreaterThanInitialArrayLengthEq3() {
+        DynamicList<String> dl = new DynamicList<>();
+        dl.add("first");
+        dl.add("second");
+        dl.add("third");
+        dl.add("fourth");
+        dl.add("fifth");
+        assertThat(dl.get(0), is("first"));
+        assertThat(dl.get(1), is("second"));
+        assertThat(dl.get(2), is("third"));
+        assertThat(dl.get(3), is("fourth"));
+        assertThat(dl.get(4), is("fifth"));
+    }
 }
