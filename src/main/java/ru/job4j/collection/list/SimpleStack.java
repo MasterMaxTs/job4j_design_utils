18769586669ca7b2fff043bd0ba471f4541e6ad2
Maxim Tsurkanov <max86ts@gmail.com>
2021-04-09 02:21:36 +0300

    5. Очередь на двух стеках [#501414]. В класс SimpleStack добавлено поле size и его геттер для определения размера стека

--- src/main/java/ru/job4j/collection/list/SimpleStack.java
+++ src/main/java/ru/job4j/collection/list/SimpleStack.java
@@ -2,12 +2,20 @@ package ru.job4j.collection.list;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> forwardLinked = new ForwardLinked<>();
+    private int size = 0;
 
     public T pop() {
+        size--;
         return this.forwardLinked.deleteFirst();
+
     }
 
     public void push(T value) {
         this.forwardLinked.addFirst(value);
+        size++;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
