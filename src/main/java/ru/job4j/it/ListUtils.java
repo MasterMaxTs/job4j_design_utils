c75279f8aa88488619f667f921cfe42db04c68b8
Maxim Tsurkanov <max86ts@gmail.com>
2021-05-02 14:22:13 +0300

    ListIterator [#501411]. Correct addAfter()

--- src/main/java/ru/job4j/it/ListUtils.java
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -21,14 +21,10 @@ public class ListUtils {
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> i = list.listIterator();
-        while (i.hasNext()) {
-            if (i.nextIndex() == index) {
-                i.next();
-                i.add(value);
-                break;
-            }
+        while (i.previousIndex() != index) {
             i.next();
         }
+        i.add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
