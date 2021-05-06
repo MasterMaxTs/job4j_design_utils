e2b837e69cf7ead19ea8b6794ad13f98a2d73e71
Maxim Tsurkanov <max86ts@gmail.com>
2021-05-06 16:27:08 +0300

    3. Переопределить только hashCode [#501441]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -22,16 +22,35 @@ public class User {
                 + '}';
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static Date setDateAndGet(
+            int year, int month, int day, int hour,
+                                                    int min, int sec, int milsec) {
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.YEAR, year);
+        date.set(Calendar.MONTH, month);
+        date.set(Calendar.DAY_OF_MONTH, day);
+        date.set(Calendar.HOUR_OF_DAY, hour);
+        date.set(Calendar.MINUTE, min);
+        date.set(Calendar.SECOND, sec);
+        date.set(Calendar.MILLISECOND, milsec);
+        return date.getTime();
+    }
+
     public static void main(String[] args) {
         User user1 = new User(
                 "Maxim",
                 1,
-                new GregorianCalendar(1986, Calendar.JANUARY, 17).getTime()
+                setDateAndGet(1986, 0, 17, 3, 15, 50, 700)
         );
         User user2 = new User(
                 "Maxim",
                 1,
-                new GregorianCalendar(1986, Calendar.JANUARY, 17).getTime()
+                setDateAndGet(1986, 0, 17, 3, 15, 50, 700)
         );
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
@@ -45,9 +64,8 @@ public class User {
         * одинаковыми ключами имеют различные значения,
         * что видно из ссылок на участок памяти в JVM.
         * Объекты попали в один и тотже бакет, т.к вычисленные
-        * автоматически значения key.hashCode() и соответственно
-        * hash(key.hashCode()) одинаковы. Метод hashCode() наследуется
-        * по умолчанию от класса Object.
+        * значения key.hashCode() и соответственно
+        * hash(key.hashCode()) одинаковы.
         * Как способ разрешения коллизии, у объектов User вызывается
         * метод equals(), унаследованный по умолчанию от класса Object.
         * Согласно default реализации метода equals() у объектов сравниваются ссылки,
