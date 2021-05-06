f527fe50384484b8222192a3ec2305e715735faf
Maxim Tsurkanov <max86ts@gmail.com>
2021-05-06 13:14:22 +0300

    2. Без переопределения equals и hashCode [#501443]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,15 +1,45 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
     private int children;
-    private Calendar birthday;
+    private Date birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    public User(String name, int children, Date birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        Calendar db = new GregorianCalendar();
+        User user1 = new User(
+                "Maxim",
+                1,
+                new GregorianCalendar(1986, Calendar.JANUARY, 17).getTime()
+        );
+        User user2 = new User(
+                "Maxim",
+                1,
+                new GregorianCalendar(1986, Calendar.JANUARY, 17).getTime()
+        );
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        for (Map.Entry<User, Object> entry
+                : map.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
     }
 }
