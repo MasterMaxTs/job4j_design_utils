2d6654d5478f18ea7d63599b8eaa2f75fec710a0
Maxim Tsurkanov <max86ts@gmail.com>
2021-06-10 14:13:56 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/config/Config.java
@@ -0,0 +1,86 @@
+package ru.job4j.io.config;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class Config {
+    private final String readPath;
+    private final String writePath;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(String readPath, String writePath) {
+        this.readPath = readPath;
+        this.writePath = writePath;
+    }
+
+    public void load() {
+        Map<String, String> hm = new HashMap<>();
+        try (BufferedReader read = new BufferedReader(
+                new FileReader(readPath));
+             BufferedWriter out = new BufferedWriter(
+                     new FileWriter(writePath)
+             )
+        ) {
+            hm = read.lines()
+                        .filter(str -> !str.equals("") && !str.startsWith("#"))
+                        .collect(Collectors.toMap(
+                                k -> {
+                                        int index = k.indexOf("=");
+                                        if (index == -1) {
+                                            throw new IllegalArgumentException();
+                                        }
+                                        String key = k.substring(0, index);
+                                        if (key.equals("")) {
+                                            throw new IllegalArgumentException();
+                                        }
+                                        return key;
+                                     },
+                                v -> {
+                                        int index = v.indexOf("=");
+                                        String value = v.substring(index + 1);
+                                        if (value.equals("")) {
+                                            throw new IllegalArgumentException();
+                                        }
+                                        return value;
+                                     }
+                                )
+                        );
+            values.putAll(hm);
+            for (String key
+                    : values.keySet()) {
+                out.write(key + "=" + values.get(key));
+                out.write(System.lineSeparator());
+            }
+            System.out.println("File has been written in " + writePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(
+                new FileReader(this.readPath)
+        )) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config(
+                "./src/main/java/ru/job4j/io/config/app.properties",
+                "./data/pair_without_comment.properties"
+                );
+        config.load();
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import ru.job4j.io.config.Config;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String readPath = "./src/main/java/ru/job4j/io/config/app.properties";
+        String writePath = "./data/pair_without_comment.properties";
+        Config config = new Config(readPath, writePath);
+        config.load();
+        assertThat(config.value("user.name"), is("maxim"));
+        assertThat(config.value(
+                "hibernate.connection.url"), is ("jdbc:postgresql://127.0.0.1:5432/trackstudio")
+        );
+        assertThat(config.value("user.age"), is (nullValue()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenKeyIsNotInPropertiesThanException() {
+        String readPath = "./src/main/java/ru/job4j/io/config/app2.properties";
+        String writePath = "./data/pair_without_comment.properties";
+        Config config = new Config(readPath, writePath);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenValueIsNotInPropertiesThanException() {
+        String readPath = "./src/main/java/ru/job4j/io/config/app3.properties";
+        String writePath = "./data/pair_without_comment.properties";
+        Config config = new Config(readPath, writePath);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenDelimiterIsNotInPropertiesThanException() {
+        String readPath = "./src/main/java/ru/job4j/io/config/app4.properties";
+        String writePath = "./data/pair_without_comment.properties";
+        Config config = new Config(readPath, writePath);
+        config.load();
+    }
+}
