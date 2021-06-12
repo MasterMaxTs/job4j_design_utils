e9e094c51140efdce89083aaae8cb5cd74da003f
Maxim Tsurkanov <max86ts@gmail.com>
2021-06-13 02:54:38 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/server/Analizy.java
+++ src/main/java/ru/job4j/io/server/Analizy.java
@@ -18,7 +18,7 @@ public class Analizy {
                             .flatMap(Arrays::stream)
                             .collect(Collectors.toList());
             StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < list.size() - 1; i++) {
+            for (int i = 0; i < list.size(); i++) {
                 if (i % 2 == 0
                         && (list.get(i).equals(SERVER_STATUS[0])
                             || list.get(i).equals(SERVER_STATUS[1]))
