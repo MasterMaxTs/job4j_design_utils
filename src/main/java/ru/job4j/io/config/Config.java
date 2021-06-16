241a6d48c88cec2d0684f8fc345187e3414e2aaa
Maxim Tsurkanov <max86ts@gmail.com>
2021-06-16 21:38:05 +0300

    1. Читаем файл конфигурации [#858 #166537]

--- src/main/java/ru/job4j/io/config/Config.java
+++ src/main/java/ru/job4j/io/config/Config.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Config {
     private final String path;
-    private final Map<String, String> values = new HashMap<>();
+    private Map<String, String> values = new HashMap<>();
 
     public Config(String path) {
         this.path = path;
@@ -14,20 +14,26 @@ public class Config {
 
     public void load() {
         try (BufferedReader read = new BufferedReader(
-                new FileReader(path));
+                new FileReader(path))
         ) {
-            List<String> list = read.lines().filter(str -> !str.equals("") && !str.startsWith("#"))
-                    .map(s -> s.split("=")).flatMap(Arrays::stream).collect(Collectors.toList());
-            if (list.isEmpty() || list.size() % 2 != 0) {
+            values = read.lines()
+                    .filter(l -> !l.equals("") && !l.startsWith("#"))
+                    .map(str -> str.split("="))
+                    .collect(
+                            Collectors.toMap(
+                                            k -> {
+                                                    if (k.length < 2) {
+                                                        throw new IllegalArgumentException(
+                                                                "Incorrect data in file: " + path
+                                                        );
+                                                    }
+                                                    return k[0];
+                                            },
+                                            v -> v[1]
+                            )
+                    );
+            if (values.isEmpty()) {
                 throw new IllegalArgumentException();
-            }
-            String key;
-            String value;
-            for (int i = 0; i < list.size() - 1; i++) {
-                key = list.get(i);
-                ++i;
-                value = list.get(i);
-                values.put(key, value);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -50,8 +56,17 @@ public class Config {
         return out.toString();
     }
 
+    public void showData() {
+        System.out.println("Config has been written!");
+        for (Map.Entry<String, String> entry
+                : values.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
+
     public static void main(String[] args) {
         Config config = new Config("./src/main/java/ru/job4j/io/config/app.properties");
         config.load();
+        config.showData();
     }
 }
