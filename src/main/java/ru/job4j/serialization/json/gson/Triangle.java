0c861734eabb5753457c107448a62e177d464162
Maxim Tsurkanov <max86ts@gmail.com>
2021-10-24 20:16:56 +0300

    2. Формат JSON [#313164 #218568].Рефакторинг

--- src/main/java/ru/job4j/serialization/json/gson/Triangle.java
+++ src/main/java/ru/job4j/serialization/json/gson/Triangle.java
@@ -2,6 +2,7 @@ package ru.job4j.serialization.json.gson;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -16,7 +17,7 @@ public class Triangle {
     private final boolean isosceles;
     private final float height;
     private final String triangleType;
-    private Point[] points;
+    private final Point[] points;
 
     public Triangle(boolean isosceles, float height,
                     String triangleType, Point[] points) {
@@ -37,6 +38,40 @@ public class Triangle {
                 + '}';
     }
 
+    private void convertObjToJsonFile(Object obj, String filePath) throws IOException {
+        final Gson gson = new GsonBuilder().create();
+        Path out = Paths.get(filePath);
+        Files.writeString(out, gson.toJson(obj));
+        System.out.println("Object has been converted in JSON file "
+                        + out.toAbsolutePath() + " successfully");
+    }
+
+    private Object initObjFromJsonFile(String filePath) throws IOException {
+        Object result = null;
+        final Gson gson = new GsonBuilder().create();
+        Path in = Paths.get(filePath);
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(in.toFile())
+        )) {
+            String jsonString = reader.lines().collect(Collectors.joining());
+            result = gson.fromJson(jsonString, Triangle.class);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private Object initObjFromJsonString(String json) {
+        Object result = null;
+        final Gson gson = new GsonBuilder().create();
+        try {
+            result = gson.fromJson(json, Triangle.class);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws IOException {
         final Triangle triangle = new Triangle(
                false,
@@ -44,34 +79,24 @@ public class Triangle {
                "acute-angled",
                 new Point[]{new Point(0, 0), new Point(0, 0), new Point(0, 0)}
         );
-        /* Преобразуем объект person в json-строку и запишим в выходной файл. */
-        final Gson gson = new GsonBuilder().create();
-        Path out = Paths.get("./src/main/java/ru/job4j/serialization/json/gson"
-                + "/TriangleSettingsDefault.json");
-        Files.writeString(out, gson.toJson(triangle));
-
-        /*Проинициализированный объект json-строкой из файла TriangleSettings
-        .json*/
-        Path input = Paths.get("./src/main/java/ru/job4j/serialization/json"
-                + "/gson/TriangleSettings.json");
-        try (BufferedReader reader = new BufferedReader(
-                new FileReader(input.toFile())
-        )) {
-            String jsonString = reader.lines().collect(Collectors.joining());
-            final Triangle triangleInit = gson.fromJson(jsonString,
-                    Triangle.class);
-            System.out.println(triangleInit);
-        }
-
+        /* Преобразуем объект triangle в json-строку и запишим в выходной файл */
+        String out = "./src/main/java/ru/job4j/serialization/json/gson/"
+            + "TriangleSettingsDefault.json";
+        triangle.convertObjToJsonFile(triangle, out);
+        /*Проинициализированный объект json-строкой из файла TriangleSettings.json*/
+        String in = "./src/main/java/ru/job4j/serialization/json/gson/"
+                + "TriangleSettings.json";
+        Triangle triangleInit = (Triangle) triangle.initObjFromJsonFile(in);
+        System.out.println(triangleInit);
         /* Модифицируем json-строку и инициализируем новый объект*/
-        final String triangleJson =
+        String triangleJson =
                 "{"
                     + "\"isosceles\":true,"
                     + "\"height\": 6.0,"
                     + "\"triangleType\":obtuse,"
                     + "\"points\":[{\"x\":1.0,\"y\":1.0}, {\"x\":5.0,\"y\":4.0}, {\"x\":9.0,\"y\":1.0}]"
                     + "}";
-        final Triangle triangleMod = gson.fromJson(triangleJson, Triangle.class);
+        Triangle triangleMod = (Triangle) triangle.initObjFromJsonString(triangleJson);
         System.out.println(triangleMod);
     }
 }
