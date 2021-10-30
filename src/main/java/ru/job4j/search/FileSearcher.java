f01dbef0d849b08d0f7b81428fce2a89e8cbceb9
Maxim Tsurkanov <max86ts@gmail.com>
2021-10-30 14:01:42 +0300

    2. Поиск файлов по критерию [#783 #221926].

---
+++ src/main/java/ru/job4j/search/FileSearcher.java
@@ -0,0 +1,121 @@
+package ru.job4j.search;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FileSearcher {
+    private static final String MASK = "mask";
+    private static final String NAME = "name";
+    private static final String REGEX = "regex";
+    private static final int POSITION = 0;
+
+    public void searchFiles(Path root, String condition, String typeSearch,
+                            Path out) throws IOException {
+        switch (typeSearch) {
+            case MASK:
+                writeResult(
+                        Search.search(
+                                root,
+                                p -> {
+                                    String notSupCharInFileName = "[^<>:«/\\| ]";
+                                    String regex = condition.replaceAll(
+                                            "\\*", notSupCharInFileName.concat("+")
+                                            )
+                                            .replaceAll(
+                                                    "\\?", notSupCharInFileName
+                                            ).replaceAll(
+                                                    "\\.", "\\\\."
+                                            );
+                                    Pattern pattern = Pattern.compile(regex);
+                                    Matcher matcher =
+                                            pattern.matcher(p.toFile().getName());
+                                    return matcher.matches();
+                                }
+                        ),
+                        out
+                );
+                break;
+            case NAME:
+                writeResult(
+                    Search.search(
+                            root,
+                            p -> {
+                                if (condition.contains(".")) {
+                                    return p.toFile().getName().equals(condition);
+                                }
+                                return p.toFile().getName().split("\\.")[POSITION].equals(condition);
+                            }
+                    ),
+                    out
+            );
+                break;
+            default:
+                writeResult(
+                        Search.search(
+                                root,
+                                p -> {
+                                    Pattern pattern = Pattern.compile(condition);
+                                    Matcher matcher =
+                                            pattern.matcher(p.toFile().getName());
+                                    return matcher.matches();
+                                }
+                        ),
+                        out
+                );
+                break;
+        }
+    }
+
+    public void writeResult(List<Path> files, Path out) throws IOException {
+        try (PrintWriter pw = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(out.toFile()))
+                )) {
+            files.forEach(pw::println);
+        }
+        System.out.println("Find " + files.size() + " file(s). "
+                + "Result has been written in file " + out.toAbsolutePath());
+    }
+
+    public ArgsName validate(String[] args) {
+        final int POS = 1;
+        if (args.length != 4) {
+            throw new IllegalArgumentException(
+                    "Incorrect entry of arguments!\n"
+                            + " Usage java -jar target/find.jar "
+                            + "-d=directory to start searching "
+                            + "-n=mask, file name, or regular expression "
+                            + "-t=search type. Value of [mask,name,regex]"
+                            + "-o=path to the file with found results"
+            );
+        }
+        File file = new File(args[0].split("=")[POS]);
+        if (!file.isDirectory() || !file.exists()) {
+            throw new IllegalArgumentException(
+                    "Directory not found!" + "Inter correct value in key <-d>");
+        }
+        String type = args[2].split("=")[POS];
+        List<String> listTypes = List.of(MASK, NAME, REGEX);
+        if (!listTypes.contains(type)) {
+            throw  new IllegalArgumentException(
+                    "Incorrect value of argument -t!\n"
+                            + "Enter one of the values [mask, name, regex] in"
+                            + " key <-t>");
+        }
+        return ArgsName.of(args);
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileSearcher searcher = new FileSearcher();
+        ArgsName jvm = searcher.validate(args);
+        Path directory = Paths.get(jvm.get("d"));
+        String condition = jvm.get("n");
+        String typeSearch = jvm.get("t");
+        Path out = Paths.get(jvm.get("o"));
+        searcher.searchFiles(directory, condition, typeSearch, out);
+    }
+}
--- src/test/java/ru/job4j/search/FileSearcherTest.java
+++ src/test/java/ru/job4j/search/FileSearcherTest.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class FilesSearcherTest {
+public class FileSearcherTest {
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
@@ -27,13 +27,19 @@ public class FilesSearcherTest {
         File file3 = temporaryFolder.newFile("log.txt");
         File target = temporaryFolder.newFile("target.txt");
         ArgsName argsName = ArgsName.of(
-                new String[]{"-d=" + root, "-n=log", "-t=name", "-o=" + target}
+                new String[]
+                        {
+                            "-d=" + root,
+                            "-n=log",
+                            "-t=name",
+                            "-o=" + target
+                        }
         );
         Path directory = Path.of(argsName.get("d"));
         String condition = argsName.get("n");
         String typeSearch = argsName.get("t");
         Path out = Path.of(argsName.get("o"));
-        FilesSearcher searcher = new FilesSearcher();
+        FileSearcher searcher = new FileSearcher();
         searcher.searchFiles(directory, condition, typeSearch, out);
         try (Scanner sc = new Scanner(out)) {
             List<String> rsl = new ArrayList<>(
@@ -59,13 +65,19 @@ public class FilesSearcherTest {
             File file3 = temporaryFolder.newFile("log.txt");
             File target = temporaryFolder.newFile("target.txt");
             ArgsName argsName = ArgsName.of(
-                new String[]{"-d=" + root, "-n=log.tmp", "-t=name", "-o=" + target}
+                new String[]
+                        {
+                            "-d=" + root,
+                            "-n=log.tmp",
+                            "-t=name",
+                            "-o=" + target
+                        }
             );
             Path directory = Path.of(argsName.get("d"));
             String condition = argsName.get("n");
             String typeSearch = argsName.get("t");
             Path out = Path.of(argsName.get("o"));
-            FilesSearcher searcher = new FilesSearcher();
+            FileSearcher searcher = new FileSearcher();
             searcher.searchFiles(directory, condition, typeSearch, out);
             try (Scanner sc = new Scanner(out)) {
                 List<String> files = new ArrayList<>();
@@ -85,14 +97,19 @@ public class FilesSearcherTest {
             File file3 = temporaryFolder.newFile("log.py");
             File target = temporaryFolder.newFile("target.txt");
             ArgsName argsName = ArgsName.of(
-                    new String[]{"-d=" + root, "-n=.+\\.\\D{2}", "-t=regex",
-                            "-o=" + target}
+                    new String[]
+                            {
+                                "-d=" + root,
+                                "-n=.+\\.py\\Z",
+                                "-t=regex",
+                                "-o=" + target
+                            }
             );
             Path directory = Path.of(argsName.get("d"));
             String condition = argsName.get("n");
             String typeSearch = argsName.get("t");
             Path out = Path.of(argsName.get("o"));
-            FilesSearcher searcher = new FilesSearcher();
+            FileSearcher searcher = new FileSearcher();
             searcher.searchFiles(directory, condition, typeSearch, out);
             try (Scanner sc = new Scanner(out)) {
                 List<String> files = new ArrayList<>();
@@ -110,16 +127,22 @@ public class FilesSearcherTest {
         File file1 = temporaryFolder.newFile("log.txt");
         File file2 = temporaryFolder.newFile("log.tmp");
         File file3 = temporaryFolder.newFile("words.tmp");
+        File file4 = temporaryFolder.newFile("bit.bmp");
         File target = temporaryFolder.newFile("target.txt");
         ArgsName argsName = ArgsName.of(
-                new String[]{"-d=" + root, "-n=**.tmp", "-t=mask",
-                        "-o=" + target}
+                new String[]
+                        {
+                            "-d=" + root,
+                            "-n=*.?mp",
+                            "-t=mask",
+                            "-o=" + target
+                        }
         );
         Path directory = Path.of(argsName.get("d"));
         String condition = argsName.get("n");
         String typeSearch = argsName.get("t");
         Path out = Path.of(argsName.get("o"));
-        FilesSearcher searcher = new FilesSearcher();
+        FileSearcher searcher = new FileSearcher();
         searcher.searchFiles(directory, condition, typeSearch, out);
         try (Scanner sc = new Scanner(out)) {
             List<String> files = new ArrayList<>();
@@ -130,11 +153,12 @@ public class FilesSearcherTest {
             сервере Travis CI */
             files.sort(String::compareTo);
             List<String> expected = List.of(
+                    file4.getAbsolutePath(),
                     file2.getAbsolutePath(),
                     file3.getAbsolutePath()
             );
             assertThat(files, is(expected));
-            assertThat(files.size(), is(2));
+            assertThat(files.size(), is(3));
         }
     }
 
@@ -142,25 +166,38 @@ public class FilesSearcherTest {
     public void whenInvalidNumberOfEntryArgumentsThanException() {
         String[] args = new String[]
                 {
-                "-d=c:/",
-                "-n=**.tmp",
-                "-t=mask"
+                    "-d=c:/",
+                    "-n=**.tmp",
+                    "-t=mask"
                 };
-        FilesSearcher searcher = new FilesSearcher();
+        FileSearcher searcher = new FileSearcher();
         searcher.validate(args);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void whenDirectoryForSearchNotExist() throws IOException {
+        File file = temporaryFolder.newFile("folder");
+        String[] args = new String[]
+                {
+                    "-d=" + file,
+                    "-n=*.?mp",
+                    "-t=mask",
+                    "-o=c:/target/files.txt"
+                };
+        FileSearcher searcher = new FileSearcher();
+        searcher.validate(args);
+    }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenInvalidEntryArgumentThanException() {
         String[] args = new String[]
                 {
-                        "-d=c:/",
-                        "-n=",
-                        "-t=regex",
-                        "-o=c:/target/files.txt"
+                    "-d=c:/",
+                    "-n=",
+                    "-t=regex",
+                    "-o=c:/target/files.txt"
                 };
-        FilesSearcher searcher = new FilesSearcher();
+        FileSearcher searcher = new FileSearcher();
         searcher.validate(args);
     }
 
@@ -168,12 +205,12 @@ public class FilesSearcherTest {
     public void whenInvalidValueInTypeSearchParameterThanException() {
         String[] args = new String[]
                 {
-                "-d=c:/",
-                "-n=**.tmp",
-                "-t=fileName",
-                "-o=c:/target/files.txt"
+                    "-d=c:/",
+                    "-n=result.txt",
+                    "-t=fileName",
+                    "-o=c:/target/files.txt"
                 };
-        FilesSearcher searcher = new FilesSearcher();
+        FileSearcher searcher = new FileSearcher();
         searcher.validate(args);
     }
 }
