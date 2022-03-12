da1a9d07ef34986926180440bfa11806bd1c2808
Maxim Tsurkanov <max86ts@gmail.com>
2022-03-12 23:38:56 +0300

    1. Создать меню. [#4748 #273147].Refactoring

--- src/main/java/ru/job4j/ood/isp/menu/Printer.java
+++ src/main/java/ru/job4j/ood/isp/menu/Printer.java
@@ -5,15 +5,15 @@ public class Printer implements MenuPrinter {
     @Override
     public String print(Menu menu) {
         StringBuilder sb = new StringBuilder();
-        for (Menu.MenuItemInfo itemInfo : menu) {
-            sb.append(
-                    "\t".repeat(
-                            itemInfo.getNumber().split("\\.").length - 1
-                    )
-            );
-            sb.append(itemInfo.getNumber()).append(" ");
-            sb.append(itemInfo.getName()).append(MenuPrinter.LS);
-        }
+        menu.forEach(i -> {
+                            sb.append(
+                                    "\t".repeat(
+                                            i.getNumber().split("\\.").length - 1)
+                                    );
+                            sb.append(i.getNumber()).append(" ");
+                            sb.append(i.getName()).append(MenuPrinter.LS);
+                            }
+        );
         return sb.toString();
     }
 }
